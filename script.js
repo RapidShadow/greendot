e937ba0add12ffb19d85cbde6ad4a06cad3797e6
David Ung <43914518+RapidShadow@users.noreply.github.com>
2022-09-04 00:07:49 -0700

    origin commit

---
+++ script.js
@@ -0,0 +1,60 @@
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+if (mm == "01") {
+today = `January ${dd}, ${yyyy}`;
+}
+else if (mm == "02") {
+  today = `February ${dd}, ${yyyy}`;
+
+}
+else if (mm == "03") {
+  today = `March ${dd}, ${yyyy}`;
+}
+else if (mm == "04") {
+  today = `April ${dd}, ${yyyy}`;
+}
+else if (mm == "05") {
+  today = `May ${dd}, ${yyyy}`;
+}
+else if (mm == "06") {
+  today = `June ${dd}, ${yyyy}`;
+}
+else if (mm == "07") {
+  today = `July ${dd}, ${yyyy}`;
+}
+else if (mm == "08") {
+  today = `August ${dd}, ${yyyy}`;
+}
+else if (mm == "09") {
+  today = `September ${dd}, ${yyyy}`;
+}
+else if (mm == "10") {
+  today = `October ${dd}, ${yyyy}`;
+}
+else if (mm == "11") {
+  today = `November ${dd}, ${yyyy}`;
+}
+else if (mm == "12") {
+  today = `December ${dd}, ${yyyy}`;
+}
+
+
+let date = document.getElementById("date");
+date.innerHTML = today;
+
+function enterName() {
+  console.log("yes");
+  let text;
+  let person = prompt("Please enter your name:", "name");
+  if (person == null || person == "") {
+    text = "Cleared to enter the campus";
+  } else {
+    text = `${person} <br> Cleared to enter the campus`;
+  }
+  document.getElementById("name").innerHTML = text;
+}
+enterName();
